5ffe661d9ddd777d826a36ccaaa3e02f97e3446e
andy179176 <andy179176@gmail.com>
2020-08-04 09:17:56 +0200

    lesson 20200803 Hotel #1 add days calculation

--- 20200731_bookingHotel/src/com/company/DateInterval.java
+++ 20200731_bookingHotel/src/com/company/DateInterval.java
@@ -24,12 +24,24 @@ public class DateInterval {
 
 
     public int getDays(){
+     /*
         int min = 1;
         int max = 100;
         if (days == 0) {
             days =min+rnd.nextInt(max-min+1);
 
         }
+        10.10.2016 ....... 03.08.2020
+
+*/
+        if (days == 0){
+            for(int i = start.getYear();i<finish.getYear();i++){
+                days+=Date.getDaysPerYear(i);
+            }
+            days-=start.daysFromNewYear();
+            days+=finish.daysFromNewYear();
+        }
+
         return days;
     }
 
